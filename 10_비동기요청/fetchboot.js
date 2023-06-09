0f31c80d62a37aab123fe7b9197a8ce6f58a7674
jaan <jwom1744@kyonggi.ac.kr>
2023-06-09 16:49:36 +0900

    비동기요청

--- 10_비동기요청/fetchboot.js
+++ 10_비동기요청/fetchboot.js
@@ -4,7 +4,36 @@ async function loadPersonList(){
     const response = await fetch(`http://localhost:8080/resttest/person-list`); //GET방식
     
     // 요청의 결과로 받은 response의 body에 담긴 json만 추출해줍니다.
-    const jsonData = await response.json();
-    console.log(jsonData);
+    const jsonArray = await response.json();
+    console.log(jsonArray);
+    console.log("----------------------")
+    for(json of jsonArray){ // 향상된 for문 형식으로 개별 json 분리
+        console.log(jsonData.id); // json 내부 자료에 대해 멤버변수처럼 호출
+        console.log(jsonData.name);
+        console.log(jsonData.age);
+    }
 
+    // -------------------------------------------------
+
+    // #personInfo 태그 얻어오기
+    const $personInfo = document.getElementById("personInfo");
+
+    // 문자열 변수 생성 후, 각 정보를 
+    // 번호 : xxx, 이름 : xxx, 나이 : xxx 형식으로 문자로 저장해주세요.
+    // 반복문을 활용해주세요.
+    let str = "";
+    for(jsonData of jsonArray){ // 향상된 for문 형식으로 개별 json 분리
+        str += `<h1>번호 : ${jsonData.id}, 이름 : ${jsonData.name}, 나이 : ${jsonData.age}</h1>`;
+    }
+    console.log(jsonArray);
+
+    // #personInfo에 innerHTML 을 이용해 끼워넣어주세요.
+    $personInfo.innerHTML = str;
 }
+            
+// getPersonList 버튼을 얻어와서 변수에 저장해주세요.
+let $button = document.getElementById("getPersonList");
+            
+// 버튼에 loadPersonList()를 클릭시 수행되도록 처리해줍니다.
+        // loadPersonList() 로 대입 시, 실행 결과자료를 해당 위치에 대입
+$button.addEventListener("click", loadPersonList);
--- 10_비동기요청/fetchmovie.js
+++ 10_비동기요청/fetchmovie.js
@@ -1,4 +1,16 @@
-async function loadBoxOffice(){
+//fetch()를 이용해서 rest서버에 요청을 넣거나 결과를 받아올 수 있습니다.
+/* async function loadMovieList(){
+    // fetch의 첫 번째 파라미터는 요청 주소입니다.
+    const response = await fetch(`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230505`); //GET방식
+    
+    // 요청의 결과로 받은 response의 body에 담긴 json만 추출해줍니다.
+    const jsonData = await response.json();
+    console.log(jsonData);
+}
+*/
 
-    const response = await fetch();
+function loadMovieList(){
+    fetch("http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230505")
+    .then((response) => response.json()) // (response)는 이전 fetch(주소)의 리턴자료
+    .then((result) => console.log(result)); // (result)는 response.json()으로 얻어진 json자료 
 }
